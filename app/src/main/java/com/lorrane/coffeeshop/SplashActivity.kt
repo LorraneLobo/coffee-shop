a015b130ec8f9e979b49245697ce2182613a672b
Lorrane Lobo <lorrane.dev@gmail.com>
2026-05-08 17:05:04 -0300

    Remove .idea folder from Git tracking

---
+++ app/src/main/java/com/lorrane/coffeeshop/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.lorrane.coffeeshop
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.lorrane.coffeeshop.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
